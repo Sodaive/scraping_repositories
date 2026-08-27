42c1d7f5c6c5bb7d60f7857646ca509ee0a10f42
suda <p.sodaive@gmail.com>
2026-08-27 11:15:12 +0330

    bug fixed

--- scraping_repository.py
+++ scraping_repository.py
@@ -13,6 +13,6 @@ for repo in repos:
     name = repo.find('h3').find('a').get_text(strip=True)
     language = repo.find(attrs = {'itemprop':'programmingLanguage'})
     language = language.get_text(strip=True) if language else 'Unknown'
-    stars = repo.find('a',attrs = {'href':re.compile('\/stargazers')})
-    stars = int(stars.get_text(strip=True)) if stars else 0
+    stars = repo.find('a',attrs = {'href':re.compile(r'/stargazers')})
+    stars = int(stars.get_text(strip=True).replace(',', '')) if stars else 0
     print(name, language, stars)
